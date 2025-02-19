2f664bf880df8dd896957f236d8e116c2ffacbf4
Vijay Suryakant Kapse <21f1000202@ds.study.iitm.ac.in>
2025-02-19 12:09:14 +0530

    Update index.go

--- api/index.go
+++ api/index.go
@@ -1,11 +1,12 @@
-package handler
+// File: api/index.go
+package main
 
 import (
 	"fmt"
 	"net/http"
 )
 
-// Handler function required by Vercel
+// Handler is automatically recognized by Vercel as the entry point
 func Handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Paisa finance tracker is running on Vercel!")
 }
