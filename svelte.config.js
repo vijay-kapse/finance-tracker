775c12bcde0036e71160b7cb5a335f301b6ab7c0
Vijay Suryakant Kapse <21f1000202@ds.study.iitm.ac.in>
2025-02-19 12:24:54 +0530

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,25 +1,13 @@
-import adapter from "@sveltejs/adapter-static";
-import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import adapter from '@sveltejs/adapter-vercel';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
-  // for more information about preprocessors
   preprocess: vitePreprocess(),
-
-  onwarn: (warning, handler) => {
-    if (warning.code.startsWith("a11y-")) return;
-    handler(warning);
-  },
-
   kit: {
-    adapter: adapter({
-      pages: "web/static",
-      assets: "web/static",
-      out: "web/static",
-      fallback: "index.html"
-    })
+    adapter: adapter() // Use the Vercel adapter with zero-config
   }
 };
 
 export default config;
+
