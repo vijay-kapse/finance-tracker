e5b27c83dcd6915788f1de00a1e05e426a784c7e
Vijay Suryakant Kapse <21f1000202@ds.study.iitm.ac.in>
2025-02-19 12:11:22 +0530

    Update index.go

--- api/index.go
+++ api/index.go
@@ -1,12 +1,12 @@
-// File: api/index.go
 package main
 
 import (
-	"fmt"
-	"net/http"
+    "fmt"
+    "net/http"
 )
 
-// Handler is automatically recognized by Vercel as the entry point
+// Handler is the entry point for Vercel
 func Handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Paisa finance tracker is running on Vercel!")
+    fmt.Fprintf(w, "Paisa finance tracker is running on Vercel!")
 }
+
